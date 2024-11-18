76abab1769bb0ef108128b53fec60522439b63a8
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-18 23:08:58 +0300

    Initial header

---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+    <h1> <strong> UOB IT College Room Booking </strong></h1>
+    <nav>
+        <ul>
+            <li> <a href = "index.php"> Main </a></li>
+            <li> <a href = "registration.php"> User registration </a></li>
+            <li> <a href = "login.php"> User Login </a></li>
+
+        </ul>
+    </nav>
+
+</header>
