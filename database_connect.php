dd949bf829574bb30b9a2643144c27bb3388bf35
RAWANIO <rawanalhussaini77@gmail.com>
2024-11-20 00:51:31 +0300

    Initial

--- database_connect.php
+++ database_connect.php
@@ -5,11 +5,9 @@ $database_name = 'IT_College_Room_Booking';
 $password = '      ';
 
 try {
-    $pdo_Statement1 = new PDO("mysql:host=$host;database_name=$database_name", $username, $password);
+    $pdo = new PDO("mysql:host=$host;database_name=$database_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Connection Unsuccessful" . $e->getMessage();
-
+    echo "Connection Unsuccessful: " . htmlspecialchars($e->getMessage());
 }
-?>
-
+?> 
