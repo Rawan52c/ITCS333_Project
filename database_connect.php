dbe99d8ab8e8825bc8996fbd39bd5af2c7f46a17
RAWANIO <rawanalhussaini77@gmail.com>
2024-12-09 17:19:46 +0300

    Updated

--- database_connect.php
+++ database_connect.php
@@ -6,7 +6,7 @@ $database_name = 'IT_College_Room_Booking';
 
 
 try {
-    $pdo = new PDO("mysql:host=$host;database_name=$database_name", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$database_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection Unsuccessful: " ($e->getMessage());
